58346c8ef2f5aa0cf4a83d718e4f3dc8f391f97a
Mohit singh <78977366+singhmohit14072002@users.noreply.github.com>
2022-01-02 10:44:01 +0530

    Add files via upload

--- project/Main.java
+++ project/Main.java
@@ -1,1 +1,11 @@
+package com.Mohit;
 
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Testing.....");
+        MyWindow w = new MyWindow();
+
+    }
+}
